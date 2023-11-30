f003eb5554c715ba9b359e35cfe37ac2e45ffa17
KDavis006 <110576696+KDavis006@users.noreply.github.com>
2023-11-30 15:21:53 -0700

    added comments to my page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
+// import the counter
 import Counter from './Components/PageViewCounter'
 
+// runs the app
 const App = () => {
   return (
+    // has the counter appear on the page
     <Counter/>
   )
 }
 
+// exports the app so that the index file can run it
 export default App
